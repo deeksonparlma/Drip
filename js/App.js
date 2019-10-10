2416032df238092c74209ecd83d3427cf7baf751
deeksonparlma <dicksonparlma@gmail.com>
2019-10-10 19:10:07 +0300

    emailing syntax changed

--- js/App.js
+++ js/App.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
         }
       }
       if (increment != 0) {
-        window.open('mailto : dickson.nyaigoti@gmail.com ?subject=DripSchool Shoe Order & body = I have purchased +'+ shoetype.toString() +" "+' shoes worth ' +" "+(price) + '\r\n' );
+        window.location.href ='mailto : dickson.nyaigoti@gmail.com ?subject=DripSchool Shoe Order & body = I have purchased +'+ shoetype.toString() +" "+' shoes worth ' +" "+"Ksh"+". "+(price);
         price = 0;
       }
   });
